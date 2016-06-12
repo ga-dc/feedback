773a54cb73a9d2db03b7303b122a84f1a9c09714
Jesse Shawl <jesse@jshawl.com>
2016-06-12 19:40:46 -0400

    add project 1 rubric

---
+++ rubrics/project-1.js
@@ -0,0 +1,35 @@
+module.exports = {
+  header: "# Project 1 Evaluation\n",
+  labels: [
+    "Incomplete",
+    "Progressing",
+    "Performing",
+    "Excelling"
+  ],
+  criteria: {
+    "Planning / Process / Submission":[
+      "No submission received, or submission does not contain basic documentation of planning (ERD, wireframes, commit history, etc)",
+      "App is submitted, with basic evidence of planning. Documentation exists, but lacks common areas such as setup instructions, description of application functionality and link to deployed application",
+      "Submission contains clear evidence of planning, adequate documentation, include all from previous category, as well as additional information such as unsolved issues.",
+      "Submission includes everything in previous category, as well as evidence of planning tools, such as Trello, and, incorporates workflows such as feature branching, code review, github issue / user story tracking, and justification of technical decisions."
+    ],
+    "Technical Requirements": [
+      "App does not render in the browser, with significant errors",
+      "Limited use of inline styles and JavaScript in HTML pages",
+      "App functions with minimal or no errors and is deployed on github pages",
+      "App is deployed with a custom domain"
+    ],
+    "Code Quality": [
+      "Minimal functional code is present",
+      "Code lacks proper formatting, includes commented out, non-functional code, or otherwise contains major issues of quality (DRY, naming, etc)",
+      "Code is well-formatted, well-named, and contains few style / quality issues",
+      "No major code quality issues, makes use of Ruby best practices appropriately, and follows techniques such as separation of concerns, abstraction, and encapsulation"
+    ],
+    "Creativity and Interface":[
+      "Zero or Minimal custom styles or interface. Evidence of scaffolded code",
+      "The app is styled and has an interface of value to the end user: it is not just a nav bar and an index page",
+      "The app displays a personal spin or creative element through its written language, functionality, purpose, and/or CSS. Submission includes proof of HTML / CSS validation",
+      "The app is fully responsive, incorporates outside technologies such as Flexbox. App incorporates modern UI themes, and adds unique flair."
+    ]
+  }
+}
